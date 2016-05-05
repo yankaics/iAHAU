495bb318b526df627b23aeda12262f70bb13df37
anderson <andersion.v@gmail.com>
2016-05-05 08:53:26 +0800

    添加社团.

---
+++ app/src/main/java/net/peacesky/iahau/model/Club.java
@@ -0,0 +1,24 @@
+package net.peacesky.iahau.model;
+
+/**
+ * Created by hc on 16-4-28.
+ */
+public class Club {
+    private String name;
+    private int imageId;
+
+    public Club(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+
+}
